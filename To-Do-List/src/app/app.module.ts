d5110c72928fcb42a6953030235225d81d6bad57
propertyofkeow <marta.sibielak92@gmail.com>
2020-06-30 00:48:36 +0200

    create main list

--- To-Do-List/src/app/app.module.ts
+++ To-Do-List/src/app/app.module.ts
@@ -6,18 +6,26 @@ import {FormsModule} from "@angular/forms";
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/materiaL/card'
 import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatExpansionModule } from "@angular/material/expansion";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import {DragDropModule} from "@angular/cdk/drag-drop";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PostsComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +34,11 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
     BrowserAnimationsModule,
     MatInputModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatExpansionModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ To-Do-List/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from "@angular/cdk/drag-drop";
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.scss']
+})
+export class PostListComponent {
+
+  todo = [
+    'Get to work',
+    'Pick up groceries',
+  ];
+
+  done = [
+    'Get up',
+    'Brush teeth',
+    'Walk dog'
+  ];
+
+  inprogress = [
+    'Get up',
+    'Check e-mail',
+    'Walk dog'
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
+
+}
